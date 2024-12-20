6c5dd07974c8c1bd93d65666b630b478b7d39bda
Rohit Jire <rohitjire55@gmail.com>
2024-12-20 22:22:16 +0100

    added streamlit form

---
+++ form.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+def process_form_data(data):
+    st.write("Form Data Received:", data)
+
+st.title("UniForMe")
+
+with st.form("uni_form"):
+    course_type = st.radio("Course Type", ["Specialized", "General Computer Science"])
+
+    language = st.radio("Preferred Language", ["English (En)", "German (De)"])
+
+    num_semesters = st.selectbox("Number of Semesters", [2, 3, 4, 5, 6, 7, 8])
+
+    university_type = st.radio("Type", ["Public", "Private"])
+
+    uni_type = st.radio("University Type", ["TU", "TH"])
+
+    city_area = st.slider("City Area Preference", 0.0, 1.0, 0.5)
+
+    job_opportunities = st.slider("Job Opportunities", 0.0, 1.0, 0.5)
+
+    additional_summary = st.text_area("Additional Summary", placeholder="Enter any additional preferences or comments here...")
+
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    form_data = {
+        "Course Type": course_type,
+        "Language": language,
+        "Number of Semesters": num_semesters,
+        "Type": university_type,
+        "University Type": uni_type,
+        "City Area": city_area,
+        "Job Opportunities": job_opportunities,
+        "Additional Summary": additional_summary
+    }
+    process_form_data(form_data)
